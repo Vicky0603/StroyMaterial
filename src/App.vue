2ede6ae0a061413b320ae03ae3d517106ddf3469
Vicky0603 <victoria.kovalyova14@gmail.com>
2020-10-04 18:52:49 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,137 @@
+<template>
+  <div id="app">
+    <Header/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header"
+
+export default {
+  components: {
+    Header
+  }
+}
+
+</script>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap');
+// font-family: 'Open Sans', sans-serif;
+
+$color-secondary: #708598;
+$color-btn: #050F19;
+$bg-btn-menu: #FED83D;
+$bg-avatar: #9B51E0;
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Open Sans', sans-serif;
+}
+a{
+  text-decoration: none;
+  color: inherit;
+  display: flex;
+  align-content: center;
+}
+button{
+  display: flex;
+  align-items: center;
+  color: $color-btn;
+  font-weight: 600;
+  font-size: 14px;
+  border: none;
+}
+.container{
+  display: flex;
+  width: 100%;
+  max-width: 1110px;
+  margin: 0 auto;
+  padding: 6px;
+}
+.small-text{
+  font-size: 12px;
+  display: flex;
+  align-items: center;
+  color: $color-secondary;
+}
+.icon-wrap{
+  padding: 0 6px;
+  display: flex;
+  align-items: center;
+}
+.btn{
+  padding: 8px 16px;
+  display: flex;
+  align-items: center;
+  border-radius: 4px;
+}
+.btn-menu{
+  background-color: $bg-btn-menu;
+}
+.btn-list{
+  border: 1px solid #ECEFF1;
+  background: #FAFBFC;
+}
+
+.user-avatar{
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  font-weight: 600;
+  font-size: 14px;
+  background-color: $bg-avatar;
+  text-transform: uppercase;
+}
+.form-search{
+  display: flex;
+  position: relative;
+  align-items: center;
+  input{
+    min-width: 526px;
+  }
+  button{
+    position: absolute;
+    right: 8px;
+    background-color: #fff;
+    i{
+      padding: 0;
+    }
+  }
+}
+input{
+  padding: 12px 14px;
+  font-size: 14px;
+  border-radius: 4px;
+  border: 1px solid #ECEFF1;
+  &::placeholder{
+    color: $color-secondary;
+  }
+}
+.search-list{
+  position: absolute;
+  top: 47px;
+  width: 100%;
+  padding: 0 5px;
+  background-color: #fff;
+  border: 1px solid #ECEFF1;
+  border-radius: 4px;
+  .item-search{
+    padding: 3px 5px;
+    border-bottom: 1px solid #ECEFF1;
+    width: 100%;
+    &:last-child{
+      border: none;
+    }
+    &:hover{
+      background-color: #ECEFF1;
+    }
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,147 @@
+<template>
+  <header>
+    <div class="top">
+      <div class="container">
+        <div class="left">
+          <div class="location">
+            <a href="#" class="small-text">
+              <i class="icon-wrap"><img src="../assets/icon/location-icon.svg" alt="Локация"></i>
+              <span>Волгоградская область</span>
+            </a>
+          </div>
+        </div>
+        <div class="right">
+          <a href="#" class="small-text">Тендеры</a>
+          <a href="#" class="small-text">База подрядчиков</a>
+          <a href="#" class="small-text">
+            Другие сервисы
+            <i class="icon-wrap"><img src="../assets/icon/chevron-icon.svg" alt="Другие сервисы"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="bottom">
+      <div class="container">
+        <router-link to="/" class="logo">
+          <img src="../assets/img/logo.svg" alt="Стройпортал">
+        </router-link>
+
+        <button class="btn btn-menu">
+          <i class="icon-wrap">
+            <img src="../assets/icon/menu-icon.svg" alt="Каталог">
+          </i>
+          Каталог
+        </button>
+
+        <div class="form-search">
+          <input type="text"
+                 placeholder="Хочу найти..."
+                 v-model="str"
+                 @keyup="getSearch">
+          <button >
+            <i class="icon-wrap">
+              <img src="../assets/icon/search-icon.svg" alt="Найти">
+            </i>
+          </button>
+
+          <div class="search-list"
+               v-if="afterSearchArr !== null">
+            <div class="item-search"
+                 v-for="item in afterSearchArr"
+                 :key="item.id"
+                 :itemSearch="item">
+              {{item.title}}
+            </div>
+          </div>
+
+        </div>
+
+        <button class="btn btn-list">
+          <i class="icon-wrap">
+            <img src="../assets/icon/list-icon.svg" alt="Список">
+          </i>
+          Список
+        </button>
+        <div class="user-avatar">
+          a
+        </div>
+      </div>
+    </div>
+
+  </header>
+</template>
+
+<style lang="scss">
+header{
+  .container{
+    justify-content: space-between;
+  }
+  .top{
+    display: flex;
+    background: #FAFBFC;
+    font-size: 12px;
+    line-height: 20px;
+    .right,
+    .left{
+      display: flex;
+      width: fit-content;
+    }
+    .right{
+      .small-text{
+        margin: 0 24px;
+        &:last-child{
+          margin-right: 0;
+        }
+      }
+    }
+  }
+  .bottom{
+    padding: 18px 0;
+    border: 1px solid #ECEFF1;
+  }
+}
+</style>
+<script>
+
+export default {
+  name: 'app-header',
+  data: () => ({
+    str: '',
+    searchArr: [
+      {
+        title: 'a',
+        id: 1
+      },
+      {
+        title: 'as',
+        id: 2
+      },{
+        title: 'asd',
+        id: 3
+      },{
+        title: 'asdf',
+        id: 4
+      },{
+        title: 'asdfg',
+        id: 5
+      },
+    ],
+    afterSearchArr: null
+  }),
+  methods: {
+    async getSearch() {
+      if(this.str !== ''){
+        this.afterSearchArr = await this.searchArr
+            .filter( item => {
+              if(item.title.toLowerCase().indexOf(this.str.toLowerCase()) !== -1){
+                return item;
+              }
+            })
+      } else {
+        this.afterSearchArr = null
+      }
+    }
+  }
+}
+</script>
